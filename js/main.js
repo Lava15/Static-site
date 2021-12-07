bb6f2ebe137187d83f3e6c26f5469e9ff2357c4e
lava15 <lava9336@gmail.com>
2021-12-07 15:29:16 +0300

    fixed mobile menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector(".hide");
 const btn = document.querySelector("#toggle-menu");
 
+const close = document.querySelector("#close-btn");
+
 btn.addEventListener("click", () => {
-  menu.classList.add("menu");
+  menu.classList.toggle("menu");
+});
+
+close.addEventListener("click", () => {
+  menu.classList.toggle("menu");
 });
