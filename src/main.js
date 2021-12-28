79d4d103c18e572c7be070682172e0b0c37e37c7
lava15 <lava9336@gmail.com>
2021-12-28 22:02:58 +0300

    add alot stuff

--- src/main.js
+++ src/main.js
@@ -18,30 +18,3 @@ close.addEventListener("click", () => {
 // Toggle menu
 
 // Slider
-
-sliderSection.addEventListener("mousedown", (e) => {
-  let isPressedDown = true;
-  cursorXSpace = e.offsetX - slider.offsetLeft;
-});
-
-sliderSection.addEventListener("mousemove", (e) => {
-  if (!isPressedDown) return;
-  e.preventDefault();
-  slider.style.left = `${e.offsetX - cursorXSpace}px`;
-  boundCards();
-});
-
-window.addEventListener("mouseup", () => {
-  isPressedDown = false;
-});
-
-const boundCards = () => {
-  const container_rect = sliderSection.getBoundingClientRect();
-  const slider_rect = slider.getBoundingClientRect();
-
-  if (parseInt(slider.style.left) > 0) {
-    slider.style.left = 0;
-  } else if (slider_rect.right < container_rect.right) {
-    slider.style.left = `-${slider_rect.width - container_rect.width}px`;
-  }
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
-const path = require("path");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const path = require("path");
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "develpment";
 module.exports = {
   entry: {
     main: "./src/main.js",
   },
-  mode: "development",
+  mode: mode,
   output: {
     filename: "[name]-bundle.js",
-    path: path.resolve(__dirname, "../dist"),
+    path: path.resolve(__dirname, "./dist"),
     publicPath: "/",
+
+    rules: [
+      {
+        test: /\.css$/i,
+      },
+    ],
   },
   devServer: {
     contentBase: "dist",
