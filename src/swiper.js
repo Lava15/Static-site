64a07d812b7e7bca15342fc39ddb84544b6ed085
lava15 <lava9336@gmail.com>
2022-01-12 20:49:31 +0300

    add parallax

---
+++ src/swiper.js
@@ -0,0 +1,22 @@
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper(".Swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
